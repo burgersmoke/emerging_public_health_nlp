a26b33ff4476339db86d758211772d82b2842635
burgersmoke <burgersmoke@gmail.com>
2025-08-24 15:37:59 -0600

    Adding notebook and small sample of synthetic sentences for animal exposure

---
+++ data/animal_exposure_sentences.py
@@ -0,0 +1,40 @@
+affirmed_animal_examples_masked = ['Patient has been in contact with snakes',
+                    'Patient was bit by an dog',
+                    'Patient touched dead raccoon in the forest',
+                    'A camel spit on the patient',
+                    """He stated he was helping a friend with his pigeons""",
+]
+
+negated_animal_examples_masked = ['Patient denies any contact with snakes',
+                   'Patient was not exposured to wild dogs on their vacation',
+                   'Patient did not ride camels on the vacation',
+                   "Denies being bit by a raccoon",
+                   "has no animals in the home",
+                   ]
+
+no_exposure_animal_examples_masked = ['Patient lives in Deer Valley',
+                       'Patient transferred from Camel Creek Hospital',
+                       'Patient received his wolf merit badge in scouts',
+                       'Patient smokes camels every day',
+                       'Patient went skiing at Raccoon Valley',
+                       ]
+                       
+affirmed_animal_examples_original = ['Patient has been in contact with wild birds',
+                    'Patient was bit by a raccoon',
+                    'Patient touched cats in the forest',
+                    'A camel spit on the patient',
+                    """He stated he was helping a friend with his farm animals""",
+]
+
+negated_animal_examples_original = ['Patient denies any contact with wild birds',
+                   'Patient did not experience any animal bites',
+                   'Patient was not exposured to bears on their vacation',
+                   'Patient did not ride camels on the vacation',
+                   'Patient has not eaten deer meat']
+
+no_exposure_animal_examples_original = ['Patient lives in Antelope Valley',
+                       'Patient transferred from Antelope Creek Hospital',
+                       'Patient smokes Camels every day',
+                       'Patient went skiing at Deer Gorge',
+                       """Writer asked veteran if he wanted to visit Wild Birds Supply store on our next visit""",
+                       ]
